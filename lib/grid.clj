71594c6be3c3c21d6f61fcdf58ae5caf5a2a7305
neeasade <neeasade@users.noreply.github.com>
2024-12-16 07:33:14 -0600

    cute neighbors function

--- lib/grid.clj
+++ lib/grid.clj
@@ -1,7 +1,8 @@
 ;; I am inventing new footguns every day
-(ns lib.grid (:require [clojure.string :as string]))
+(ns lib.grid
+  (:require [clojure.string :as string]))
 
-(def grid (parse "123\n456\n789"))
+;; todo: dir-look?
 
 (defn parse [input]
   (let [grid (mapv vec (string/split-lines input))]
@@ -18,6 +19,9 @@
             :when ~condition]
         [~'x ~'y]))))
 
+(defn look [grid [x y]]
+  (-> (:grid grid) (nth y []) (nth x nil)))
+
 (defn gassoc [grid coord value]
   (assoc-in grid `[:grid ~@(reverse coord)] value))
 
@@ -29,9 +33,6 @@
             (gupdate g point f))
           grid
           (points grid)))
-
-(defn look [grid [x y]]
-  (-> (:grid grid) (nth y []) (nth x nil)))
 
 (defn within? [grid [x y]]
   (and (< -1 x (:width grid))
@@ -48,7 +49,30 @@
           (map (partial apply str) (:grid grid)))
     (println (string/replace line " " "-"))))
 
+;; 123
+;; 4@6
+;; 789
+(defn neighbors
+  "return surrounding cells in the form [[x y] look]"
+  ([grid coord] (neighbors grid coord 12346789))
+  ([grid coord neighbors]
+   (let [coords (map (fn [i] (get [[-1 -1] [0 -1] [1 -1] [-1 0] [0 0] [1 0] [-1 1] [0 1] [1 1]] (dec i)))
+                     (map (comp Integer/parseInt str) (seq (str neighbors))))]
+     (map (fn [dir]
+            ((juxt identity (partial look grid))
+             (mapv + coord dir)))
+          coords)))
+  ([grid coord grabs look-val]
+   (keep (fn [[coord v]]
+           (when (= v look-val)
+             coord))
+         (neighbors grid coord grabs))))
+
 (comment
+  (neighbors (parse "123\n456\n789") [1 1] 123)
+
+  (neighbors (parse "123\n456\n789") [1 1] 123 \2)
+
   (map (partial look (parse "123\n456\n789"))
        (points (parse "123\n456\n789")))
 
